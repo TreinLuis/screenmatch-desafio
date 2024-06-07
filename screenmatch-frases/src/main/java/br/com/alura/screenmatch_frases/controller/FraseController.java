202b77e195538575ca5b72b0e3b82ab34eccdb71
Luís Trein <124221589+TreinLuis@users.noreply.github.com>
2024-06-07 07:28:50 -0300

    Add files via upload

---
+++ screenmatch-frases/src/main/java/br/com/alura/screenmatch_frases/controller/FraseController.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch_frases.controller;
+
+import br.com.alura.screenmatch_frases.FraseDTO;
+import br.com.alura.screenmatch_frases.model.Frase;
+import br.com.alura.screenmatch_frases.service.FraseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FraseController {
+
+    @Autowired
+    private FraseService servico;
+
+    @GetMapping("/series/frases")
+    public FraseDTO obterFraseAleatoria(){
+        return servico.obterFraseAleatoria();
+    }
+
+}
